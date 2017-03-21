05b9db4ee8dfe44648c932239cf2f54de4473a11
Dustin Deus <deusdustin@gmail.com>
2017-03-21 12:56:56 +0100

    Update addStub.js

--- addStub.js
+++ addStub.js
@@ -3,8 +3,13 @@
 const Sinon = require('sinon')
 
 class AddStub {
-  static stub(hemera, pattern, error, args) {
-    return Sinon.stub(hemera, 'add').withArgs(pattern).callsArgOnWith(1, hemera, error, args)
+  static stub(hemera, pattern, request, cb) {
+    const payload = hemera.router.lookup(pattern)
+    if(payload) {
+      payload.action.call(hemera, request, cb)
+      return payload
+    }
+    throw new Error('Pattern not found. Please check that you added your server method before you stub it.')
   }
 }
 
