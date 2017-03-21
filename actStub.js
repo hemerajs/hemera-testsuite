33a0be229e425874d46eecb7c15f458bd4beb9fd
Dustin Deus <deusdustin@gmail.com>
2017-03-21 13:25:35 +0100

    Update actStub.js

--- actStub.js
+++ actStub.js
@@ -1,10 +1,15 @@
 'use strict'
 
 const Sinon = require('sinon')
+let stub = null
 
 class ActStub {
   static stub(hemera, pattern, error, args) {
-    return Sinon.stub(hemera, 'act').withArgs(pattern).callsArgOnWith(1, hemera, error, args)
+    if (!stub) {
+      stub = Sinon.stub(hemera, 'act')
+    }
+
+    return stub.withArgs(pattern).callsArgOnWith(1, hemera, error, args)
   }
 }
 
