4d52bf5ef1284a95332b89e44bb1e5c8e1fa5f16
Dustin Deus <deusdustin@gmail.com>
2017-12-22 23:50:02 +0100

    add tests

--- test/nats.spec.js
+++ test/nats.spec.js
@@ -24,6 +24,28 @@ describe('NATS Transport fake', function() {
     })
   })
 
+  it('Should fake multiple request', function(done) {
+    const nats = new Nats()
+    const hemera = new Hemera(nats)
+    hemera.ready(function() {
+      hemera.add(
+        {
+          topic: 'math',
+          cmd: 'add'
+        },
+        req => req.a + req.b
+      )
+      Promise.all([
+        hemera.act(`topic:math,cmd:add,a:1,b:2`),
+        hemera.act(`topic:math,cmd:add,a:10,b:20`)
+      ]).then((result) => {
+        expect(result[0]).to.be.equals(3)
+        expect(result[1]).to.be.equals(30)
+        done()
+      })
+    })
+  })
+
   /**
    * The asterisk character (*) matches any token at any level of the subject.
    */
@@ -130,6 +152,24 @@ describe('NATS Transport fake', function() {
       hemera.act(`topic:math,cmd:add,a:1,b:2,pubsub$:true`, () => {
         done()
       })
+    })
+  })
+
+  it('Should fake multiple publish', function(done) {
+    const nats = new Nats()
+    const hemera = new Hemera(nats, { logLevel: 'info' })
+    hemera.ready(function() {
+      hemera.add(
+        {
+          topic: 'math',
+          cmd: 'add'
+        },
+        req => {}
+      )
+      Promise.all([
+        hemera.act(`topic:math,cmd:add,a:1,b:2,pubsub$:true`),
+        hemera.act(`topic:math,cmd:add,a:1,b:2,pubsub$:true`)
+      ]).then(() => done())
     })
   })
 
