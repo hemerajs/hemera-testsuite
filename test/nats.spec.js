92876a0d71def3de48ddd5f01436f9a9c3db34b6
Dustin Deus <deusdustin@gmail.com>
2017-12-23 00:09:26 +0100

    improve tests

--- test/nats.spec.js
+++ test/nats.spec.js
@@ -179,6 +179,7 @@ describe('NATS Transport emulation', function() {
     hemera.ready(function() {
       hemera.act(`topic:math,cmd:add,a:1,b:2`, (err, resp) => {
         expect(err).to.be.exists()
+        expect(err.message).to.be.equal('Timeout')
         done()
       })
     })
