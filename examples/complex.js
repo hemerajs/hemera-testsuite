41892a0af4a92ad4faf190e53339c87a8dca0119
Dustin Deus <deusdustin@gmail.com>
2017-12-23 00:06:48 +0100

    add complex example

---
+++ examples/complex.js
@@ -0,0 +1,77 @@
+const Hemera = require('nats-hemera')
+const Nats = require('../nats')
+
+const nats = new Nats()
+const hemera = new Hemera(nats)
+
+hemera.ready(() => {
+  hemera.add(
+    {
+      topic: 'math.add'
+    },
+    function(req, cb) {
+      this.act({
+        topic: 'math',
+        cmd: 'sub',
+        a: 100,
+        b: 20
+      })
+
+      this.act(
+        {
+          topic: 'math.constants.pi',
+          cmd: 'sub'
+        },
+        function(err, resp) {
+          if (err) {
+            return
+          }
+
+          this.act(
+            {
+              topic: 'math',
+              cmd: 'sub',
+              a: 100,
+              b: resp
+            },
+            function(err, resp) {
+              if (err) {
+                cb(err)
+                return
+              }
+              cb(null, resp)
+            }
+          )
+        }
+      )
+    }
+  )
+
+  hemera.add(
+    {
+      topic: 'math',
+      cmd: 'sub'
+    },
+    function(req, cb) {
+      cb(null, req.a - req.b)
+    }
+  )
+
+  hemera.add(
+    {
+      topic: 'math.constants.*'
+    },
+    () => Math.PI
+  )
+
+  hemera.act(
+    {
+      topic: 'math.add',
+      a: 1,
+      b: 20
+    },
+    function(err, resp) {
+      console.log(err, resp)
+    }
+  )
+})
